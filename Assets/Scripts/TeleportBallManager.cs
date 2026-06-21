86b528bd533d81a6d7d0332aef9cc547d3c5df9d
mmlago <mmlago0@gmail.com>
2026-06-21 16:17:50 -0300

    Menu Terminao

--- Assets/Scripts/TeleportBallManager.cs
+++ Assets/Scripts/TeleportBallManager.cs
@@ -14,16 +14,8 @@ public class TeleportBallManager : MonoBehaviour
         // Comparamos si el GameObject que entró es exactamente el que pasaste por Inspector
         if (other.gameObject == targetObject)
         {
-            // 1. Teletransportamos la pelota a la posición del spawnpoint
+           
             targetObject.transform.position = spawnPoint.position;
-
-            // 2. Frenamos la inercia (físicas) para que no salga disparada con el impulso anterior
-            Rigidbody rb = targetObject.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.linearVelocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
-            }
 
             Debug.Log($"¡Objeto {targetObject.name} teletransportado con éxito!");
         }
